a763f3441061a3f17e01d8608bf85b1f3c63b2e3
amchuaStudent <amchua.student@ua.edu.ph>
2023-11-30 14:47:58 +0800

    first commit

---
+++ src/main/java/com/example/sql_project2/SqlProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.sql_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sql_project2/SqlProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
